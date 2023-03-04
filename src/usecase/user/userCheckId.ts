0650f28d15adca860f07336dc94cbc4b3b0af139
carlosbecker2077 <carlos.becker663@gmail.com>
2023-03-04 17:25:59 -0300

    deleted this

--- src/usecase/user/userCheckId.ts
+++
@@ -1,9 +0,0 @@
-import { UnauthorizedError } from '../../helpers/api-erros';
-
-export function userCheckid(id: any) {
-    if (!id) {
-        throw new UnauthorizedError();
-    }
-
-    return id;
-}
